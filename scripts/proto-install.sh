19f67dd33766a0f3c8751bcf69ae73554dc6b5d4
majusko <mariokapustaa@gmail.com>
2021-01-17 23:43:43 +0100

    File version fix.

--- scripts/proto-install.sh
+++ scripts/proto-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PROTOBUF_VERSION=3.14.0
+PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
 pushd /home/travis || exit
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip "${PROTOC_FILENAME}"
